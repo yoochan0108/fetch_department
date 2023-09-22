9a39ca6193670007a5beaf01c09ceb63c6f2a0f1
yoochan0108 <yoochan0108@naver.com>
2023-09-22 09:44:41 +0900

    비동기 데이터로 다이나믹 돔 생성

--- main.js
+++ main.js
@@ -8,6 +8,7 @@
   작업시간이 오래걸리는 업무를 수행할때 프로미스로 반환하면
   해당 작업완료이후 동기적으로 다음 작업을 선형화해서 수행 (es5)
 */
+const section = document.querySelector('section');
 fetch('DB/department.json')
 	.then((data) => {
 		//첫번째 then구문에 받아진 promise를 다시 바로 리턴하면
@@ -15,7 +16,18 @@ fetch('DB/department.json')
 		return data.json();
 	})
 	.then((json) => {
-		console.log(json);
+		console.log(json.members);
+		let tags = '';
+		json.members.map((data) => {
+			tags += `
+        <atricle>
+          <div class='pic'>
+            <img src='img/${data.pic}' >
+          </div>
+        </article>
+      `;
+		});
+		section.innerHTML = tags;
 	})
 
 	.catch((err) => {
