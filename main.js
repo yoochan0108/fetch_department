a61e9423e6f2292849d2cdc6a23f46a56bcd8289
yoochan0108 <yoochan0108@naver.com>
2023-09-22 09:36:58 +0900

    retru하여 then구문에서 해당 데이터를 동기적으로 활용

--- main.js
+++ main.js
@@ -8,18 +8,16 @@
   작업시간이 오래걸리는 업무를 수행할때 프로미스로 반환하면
   해당 작업완료이후 동기적으로 다음 작업을 선형화해서 수행 (es5)
 */
-fetch('DB/department.json2')
-	//fetch가 반환한 프로미스를 동기적으로 호출
+fetch('DB/department.json')
 	.then((data) => {
-		//이전 메서드에서 반환된 프로미스가 fullfiled 상태일때실행
-		console.log(data); //json형태로 parsing되지 않은 raw data
-		const result = data.json(); //json형태로 변환시 promise로 반환
-		result.then((json) => {
-			console.log(json); //json로 반환된 데이터를 then으로 확인
-		});
+		//첫번째 then구문에 받아진 promise를 다시 바로 리턴하면
+		//두번째 then구문에서 해당 데이터를 동기적으로 활용가능
+		return data.json();
 	})
+	.then((json) => {
+		console.log(json);
+	})
+
 	.catch((err) => {
-		//이전메서드에서 반환된 프라미스가 rejected 상태일때 실행
-		//오류가 발생했을때 runtime에러가 발생하지 않도록 에외사항을 준비
 		console.log(err);
 	});
