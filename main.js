9cc8767eafb15b8e7b9a59214214eeafd02439b0
yoochan0108 <yoochan0108@naver.com>
2023-09-22 10:19:49 +0900

    카드 스타일링

--- main.js
+++ main.js
@@ -10,23 +10,22 @@
 */
 const section = document.querySelector('section');
 fetch('DB/department.json')
-	.then((data) => {
-		//첫번째 then구문에 받아진 promise를 다시 바로 리턴하면
-		//두번째 then구문에서 해당 데이터를 동기적으로 활용가능
-		return data.json();
-	})
+	.then((data) => data.json())
 	.then((json) => {
 		console.log(json.members);
 		let tags = '';
 		json.members.map((data) => {
 			tags += `
-        <atricle>
+        <article>
           <div class='pic'>
             <img src='img/${data.pic}' >
           </div>
+            <h2>${data.name}</h2>
+            <p>${data.position}</p>
         </article>
       `;
 		});
+
 		section.innerHTML = tags;
 	})
 
